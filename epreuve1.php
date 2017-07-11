564351e4c26fe6348f9fdb9ded34b2be755f01eb
CharlGiup <giupponi.charlene@gmail.com>
2017-07-11 17:07:29 +0200

    Add files via upload

---
+++ epreuve1.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+use Zttp\Zttp;
+
+//récupérer les informations de la page prog1.php
+
+
+//ouvrir une url sous forme verifpr1.php?solution=nombre
+
+
+
+ ?>
